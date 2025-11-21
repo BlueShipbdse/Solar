a19855ac8526227dd520bf5666ce3e78c97544cd
BlueShip <blueshipbdse@gmail.com>
2025-11-21 02:02:19 -0600

    Fixed world time argument.
    Signed-off-by: BlueShip <blueshipbdse@gmail.com>

--- src/main/java/com/blueship/solar/command/arguments/WorldTimeArgumentType.java
+++ src/main/java/com/blueship/solar/command/arguments/WorldTimeArgumentType.java
@@ -3,23 +3,21 @@ package com.blueship.solar.command.arguments;
 import com.blueship.solar.Solar;
 import com.blueship.solar.WorldTime;
 import com.blueship.solar.util.SuggestionUtil;
+import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import io.papermc.paper.command.brigadier.MessageComponentSerializer;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
-import net.kyori.adventure.key.Key;
-import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 public class WorldTimeArgumentType implements CustomArgumentType.Converted<WorldTime, String> {
+    public static final DynamicCommandExceptionType INVALID_WORLD = new DynamicCommandExceptionType(obj -> new LiteralMessage(obj + " is not a valid world."));
     private WorldTimeArgumentType() {}
 
     public static WorldTimeArgumentType worldTime() {
@@ -33,15 +31,11 @@ public class WorldTimeArgumentType implements CustomArgumentType.Converted<World
 
     @Override
     public <S> @NotNull CompletableFuture<Suggestions> listSuggestions(@NotNull CommandContext<S> context, SuggestionsBuilder builder) {
-        SuggestionUtil.trimInvalids(Solar.getHandler().getWorlds(), (worldTime -> worldTime.getWorld().key().value()), builder.getRemainingLowerCase())
-                      .forEach(builder::suggest);
-        return builder.buildFuture();
-
+        return SuggestionUtil.suggestTrimmedValues(builder, Solar.getHandler().getWorlds(), (worldTime -> worldTime.getWorld().getName())).buildFuture();
     }
 
     @Override
     public WorldTime convert(String nativeType) throws CommandSyntaxException {
-        return Solar.getHandler().getWorld(nativeType).orElseThrow(() -> new SimpleCommandExceptionType(MessageComponentSerializer.message().serialize(
-                Component.text(nativeType + " is not a valid world."))).create());
+        return Solar.getHandler().getWorld(nativeType).orElseThrow(() -> INVALID_WORLD.create(nativeType));
     }
 }
